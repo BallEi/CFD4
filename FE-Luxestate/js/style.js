1e3dea0a68dfb565b9ecd4846f83cf3d6efdb44b
hoanghieu07021@gmail.com <hoanghieu07021@gmail.com>
2022-03-11 17:59:26 +0700

    first commit

---
+++ FE-Luxestate/js/style.js
@@ -0,0 +1,21 @@
+
+
+// function Chanle ( n ){
+//     if (n % 2 == 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// console.log(Chanle(5));
+
+let a = 1 ;
+let b = 2 ;
+
+if ( a < b ) {
+    console.log('A < B');
+} else {
+    console.log('A > B');
+}
